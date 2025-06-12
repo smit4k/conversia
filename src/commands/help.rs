5b0dd569366edf771139cf549625ae626e5156fe
smit4k <smit.patil.usa@gmail.com>
2025-06-11 21:08:56 -0400

    Add GitHub issue button to help command

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -1,4 +1,5 @@
-use poise::{serenity_prelude as serenity, CreateReply};
+use poise::{serenity_prelude as serenity};
+use ::serenity::{all::{CreateActionRow, CreateButton}};
 use crate::{Context, Error};
 
 #[poise::command(slash_command, prefix_command)]
@@ -14,10 +15,26 @@ pub async fn help(ctx: Context<'_>) -> Result<(), Error> {
         .field("/audio_meta", "Extract metadata from MP3 files (title, artist, album, year, genre).", false)
         .field("/about", "Learn more about the Conversia bot.", false)
         .field("/ping", "Check the bot's latency", false)
-        .field("/help", "Shows you this response", false);
+        .field("/help", "Shows you this response", false)
+        .footer(
+            serenity::builder::CreateEmbedFooter::new("Need help? Reach out on GitHub!")
+        );
 
-        let reply = CreateReply::default().embed(embed);
-        ctx.send(reply).await?;
+    let mut issue_button = CreateButton::new_link("https://github.com/smit4k/conversia/issues");
+    issue_button = issue_button.label("Found a bug? Create an Issue");
+    issue_button = issue_button.emoji(serenity::model::prelude::ReactionType::Custom {
+        animated: false,
+        id: serenity::model::prelude::EmojiId::new(1382099046654677073),
+        name: Some("github_white".to_string()),
+    });
+
+    let action_row = CreateActionRow::Buttons(vec![issue_button]);
+
+    let reply = poise::CreateReply::default()
+        .embed(embed)
+        .components(vec![action_row]);
+
+    ctx.send(reply).await?;
 
     Ok(())
 }
