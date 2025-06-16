3ac7a9c11377e60bdf01080ecf869271517d328c
smit4k <smit.patil.usa@gmail.com>
2025-06-16 17:13:24 -0400

    Use ConversiaGithubBanner.png as image for about command

--- src/commands/about.rs
+++ src/commands/about.rs
@@ -10,6 +10,7 @@ pub async fn about(ctx: Context<'_>) -> Result<(), Error> {
     let embed = CreateEmbed::default()
         .title("About Conversia")
         .description("Conversia is a powerful, multi-purpose file utility bot written in Rust using the serenity and poise frameworks")
+        .image("https://raw.githubusercontent.com/smit4k/conversia/master/assets/ConversiaGithubBanner.png")
         .field("Legal", "[Terms of Service](https://github.com/smit4k/conversia/blob/96ff6ecd80e7220269e19468cca3141eb4fef25b/TERMS_OF_SERVICE.md)\n[Privacy Policy](https://github.com/smit4k/conversia/blob/96ff6ecd80e7220269e19468cca3141eb4fef25b/PRIVACY_POLICY.md)", false);
         
     let mut github_button = CreateButton::new_link("https://github.com/smit4k/conversia");
